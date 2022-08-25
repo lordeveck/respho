d13edf167905abc16e0ac942994f0f7b8e258d38
unknown <emincan.kirmizi@isimplatform.io>
2022-08-25 16:10:38 +0300

    removed pixel libs

--- src/components/DailyImage/DailyImage.js
+++ src/components/DailyImage/DailyImage.js
@@ -1,42 +1,46 @@
-import { useEffect, useMemo, useState } from 'react';
-import { ImagePixelated } from 'react-pixelate';
-import { FadeLoader } from 'react-spinners';
+import { useEffect, useMemo, useRef } from 'react';
 import './DailyImage.css';
 
 function DailyImage(props) {
+    const imgCanvas = useRef(null);
     const resolutionSize = useMemo(() => {
-        return [30, 25, 15, 10, 5, 3, 1];
+        return [0.03, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5];
     }, []);
-
-    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         if (!props.dailyImageSrc) return;
 
+        const context = imgCanvas.current.getContext('2d');
+
+        context.clearRect(0, 0, imgCanvas.current.width, imgCanvas.current.height);
+
         const daily_image = new Image();
 
         daily_image.onload = () => {
-            setIsLoading(false);
+            const w = imgCanvas.current.width * resolutionSize[props.answers.length];
+            const h = imgCanvas.current.height * resolutionSize[props.answers.length];
+
+            context.drawImage(daily_image, 0, 0, w, h)
+
+            context.msImageSmoothingEnabled = false;
+            context.mozImageSmoothingEnabled = false;
+            context.webkitImageSmoothingEnabled = false;
+            context.imageSmoothingEnabled = false;
+
+            context.drawImage(imgCanvas.current, 0, 0, w, h, 0, 0, imgCanvas.current.width, imgCanvas.current.height);
         };
 
         daily_image.src = props.dailyImageSrc;
-    }, [props.dailyImageSrc, props.answers]);
+    }, [props.dailyImageSrc, props.answers, resolutionSize]);
 
     return (
         <div className='daily-question'>
-            <FadeLoader color={"hsl(139deg 27% 49%)"} loading={isLoading}></FadeLoader>
-            {
-                !isLoading ?
-                    <div>
-                        <div className='resolution-marker'>
-                            <span>{`${resolutionSize[props.answers.length]} X ${resolutionSize[props.answers.length]}`}</span>
-                        </div>
-                        <ImagePixelated src={props.dailyImageSrc} width={170} height={250} pixelSize={resolutionSize[props.answers.length]} fillTransparencyColor={"grey"} />
-                        <br></br>
-                        {props.children}
-                    </div>
-                    : null
-            }
+            <div className='resolution-marker'>
+                <span>{`${resolutionSize[props.answers.length]} X ${resolutionSize[props.answers.length]}`}</span>
+            </div>
+            <canvas ref={imgCanvas} id="viewport" className='img-canvas' width={170} height={250}></canvas>
+            <br></br>
+            {props.children}
         </div>
     );
 }
